4a04ad85bc1bd4c62bf6cc0e9c52e2cae1d1d181
EITAPORRA2 <135403977+EITAPORRA2@users.noreply.github.com>
2023-10-11 11:35:06 -0300

    Update FruitBattleGround.lua

--- FruitBattleGround.lua
+++ FruitBattleGround.lua
@@ -29,7 +29,7 @@ for _, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
 end
  
 
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/leonardo04131/Bottest/main/Rayfield.lua'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/UniverseHubScripts/Librarys/main/Rayfield.lua'))()
 local Window = Rayfield:CreateWindow({
     Name = string.format("Universe Hub | %s", GameName),
     LoadingTitle = string.format("Universe Hub", GameName),
